988b330f3142a23cb7640c00d197111a983c66a6
attarmau <87364730+attarmau@users.noreply.github.com>
2023-03-25 14:41:16 +0800

    Create realtime.py

---
+++ realtime.py
@@ -0,0 +1,7 @@
+#collect oCare Pro 100 Watch data from 
+from bluepy import btle
+from datatime import datatime 
+import csv
+service_uuid = '1822'
+address = 'd6:7c:c2:48:4e:06'
+p = btle.Peripheral
