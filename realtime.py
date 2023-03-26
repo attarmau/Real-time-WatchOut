1a34197c2720ccceac10809a27104c6eb8ebce21
attarmau <87364730+attarmau@users.noreply.github.com>
2023-03-26 10:05:56 +0800

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -1,4 +1,5 @@
 #collect oCare Pro 100 Watch data from 
+
 from bluepy import btle
 from datatime import datatime 
 import csv
@@ -9,3 +10,15 @@ p.setDelegate(MyDelegate())
 svc = p.get.ServiceByUUID(servic_uuid)
 notify = svc.getCharacteristics()[0]
 notify_handle = notify.getHandle() + 1                               
+p.writeCharacterisitic(notify_handle, b'\x01\x00')
+
+
+#data storage on a computer
+
+classMyDelegate(btle.DefaultDelegate)
+  def__init__(self)
+    btle.DefaultDelegate.__init__(self)
+def handleNotification(self, cHandle, data):
+hex = list()
+for a Char in data:
+  
