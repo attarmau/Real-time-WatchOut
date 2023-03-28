0f8f55b7b7d78abb39efcc4f1db4d3d32b524366
attarmau <87364730+attarmau@users.noreply.github.com>
2023-03-29 00:14:24 +0800

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -9,3 +9,8 @@ def animate(i):
   y1 = data['HR']
   y2 = data['SpO2']
   plt.cla()
+  plt.plot(x, y1, label = 'HR')
+  plt.plot(x, y2, label = 'SpO2')
+  plt.legend(loc='lower right')
+ani = FuncAnimation(plt.gcf(), animate, interval = 1000)
+plt.show()
