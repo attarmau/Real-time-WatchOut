ac38e9556c6d3cc98bbc13a36ca58816887f7e33
attarmau <87364730+attarmau@users.noreply.github.com>
2023-08-14 22:22:33 +0800

    Update visualise.py

--- visualise.py
+++ visualise.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
+plt.style.use('fivethirtyeight')
+
+def animate(i):
+    data = pd.read_csv('data.csv')
+    x = data['Time']  # Assuming 'Time' is the column name in your CSV
+    y1 = data['HR']
+    y2 = data['SpO2']
+    plt.cla()
+    plt.plot(x, y1, label='HR')
+    plt.plot(x, y2, label='SpO2')
+    plt.legend(loc='lower right')
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+plt.tight_layout()  # Add this line to improve layout
+plt.show()
